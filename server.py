91bcfa9cdbaa892bb185cb61386ea8f701689735
Royal Garg <royalgarg36@gmail.com>
2024-04-17 10:14:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 import sys
 import time
 import sys
@@ -163,4 +167,18 @@ def func(input_file, contents_file):
   return result
   # files.download("Your Keywords Clustered.csv")
 
-anvil.server.wait_forever()
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
